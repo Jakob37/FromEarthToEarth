bd14f2ce27312e9e88189361ffeed6ca9948c253
jakob <jakob.willforss@hotmail.com>
2016-12-26 22:49:14 +0100

    Setup of platformer movement and physics

---
+++ Assets/InfoText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InfoText : MonoBehaviour {
+
+    public Player player;
+
+    private Text text_object;
+
+	void Start () {
+        text_object = GetComponent<Text>();
+	}
+	
+	void Update () {
+        text_object.text = string.Format("Jump: {0}\nGrounded: {1}", player.is_jumping, player.is_grounded);
+	}
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,13 +3,68 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public float gravity = -9;
+    public float move_force = 10;
+    public float max_speed = 5;
+    public float jump_force = 100;
+    public Transform ground_check;
+
+    [HideInInspector] public bool facing_right = true;
+    [HideInInspector] public bool is_jumping = false;
+    [HideInInspector] public bool is_grounded = false;
+    private Animator anim;
+    private Rigidbody2D rigi;
+
+    void Awake() {
+        anim = GetComponent<Animator>();
+        rigi = GetComponent<Rigidbody2D>();
+    }
+
+    void Start () {
+
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+
+        is_grounded = Physics2D.Linecast(transform.position, ground_check.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (Input.GetButtonDown("Jump") && is_grounded) {
+            is_jumping = true;
+            print("Is jumping");
+        }
+
+        // UpdateGravity();
+
 	}
+
+    void FixedUpdate() {
+
+        float h = Input.GetAxis("Horizontal");
+        if (h * rigi.velocity.x < max_speed) {
+            rigi.AddForce(Vector2.right * h * move_force);
+        }
+
+        if (Mathf.Abs(rigi.velocity.x) > max_speed) {
+            rigi.velocity = new Vector2(Mathf.Sign(rigi.velocity.x) * max_speed, rigi.velocity.y);
+        }
+
+        if (h > 0 && !facing_right) {
+            Flip();
+        }
+        else if (h < 0 && facing_right) {
+            Flip();
+        }
+
+        if (is_jumping) {
+            rigi.AddForce(new Vector2(0f, jump_force));
+            is_jumping = false;
+        }
+    }
+
+    private void Flip() {
+        facing_right = !facing_right;
+        Vector3 the_scale = transform.localScale;
+        the_scale *= -1;
+        transform.localScale = the_scale;
+    }
 }
