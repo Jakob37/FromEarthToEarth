1ebd5fc5532293ef6cfede5f9ea125b20cd315a3
jakob <jakob.willforss@hotmail.com>
2017-01-09 20:37:33 +0100

    Platform movement adjustments and movement controller refactor

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour {
+
+
+    private Player player;
+    private Rigidbody2D rigi;
+
+    void Start() {
+        player = FindObjectOfType<Player>();
+        rigi = player.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    public void EdgeCheck() {
+
+        float edge_margin = 0.02f;
+
+        var viewport_pos = Camera.main.WorldToViewportPoint(transform.position);
+        viewport_pos.x = Mathf.Clamp(viewport_pos.x, edge_margin, 2);
+        transform.position = Camera.main.ViewportToWorldPoint(viewport_pos);
+    }
+
+    public void UpdateJump() {
+
+        if (player.is_grounded) {
+            player.is_jumping = true;
+        }
+        else if (player.remaining_jumps > 0) {
+            player.is_jumping = true;
+            player.remaining_jumps -= 1;
+            rigi.velocity = new Vector2(rigi.velocity.x, 0);
+        }
+
+        if (player.is_jumping) {
+            rigi.AddForce(new Vector2(0f, player.jump_force));
+            player.is_jumping = false;
+            player.is_grounded = false;
+        }
+    }
+
+    public void UpdateHorizontalMovement() {
+
+        float h = Input.GetAxis("Horizontal");
+        if (h * rigi.velocity.x < player.max_speed) {
+            rigi.AddForce(Vector2.right * h * player.move_force);
+        }
+        if (Mathf.Abs(rigi.velocity.x) > player.max_speed) {
+            rigi.velocity = new Vector2(Mathf.Sign(rigi.velocity.x) * player.max_speed, rigi.velocity.y);
+        }
+
+        if (h > 0 && !player.facing_right) {
+            Flip();
+        }
+        else if (h < 0 && player.facing_right) {
+            Flip();
+        }
+    }
+
+    public bool CheckGrounded() {
+
+        return Physics2D.OverlapCircle(player.ground_check.position, player.ground_radius, player.what_is_ground);
+    }
+
+    public void Flip() {
+        player.facing_right = !player.facing_right;
+        Vector3 the_scale = transform.localScale;
+        the_scale.x *= -1;
+        transform.localScale = the_scale;
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float move_force = 10;
+    public float max_speed = 5;
+    public float jump_force = 100;
+
+    public int air_jumps = 1;
+    [HideInInspector] public int remaining_jumps;
+
+    [HideInInspector] public bool facing_right = true;
+    [HideInInspector] public bool is_jumping = false;
+    [HideInInspector] public bool is_grounded = false;
+    private Animator anim;
+    private Rigidbody2D rigi;
+
+    private LevelLogic level_logic;
+
+    public Transform ground_check;
+    public float ground_radius = 0.2f;
+    public LayerMask what_is_ground;
+
+    private PlatformController platform_controller;
+
+
+    void Awake() {
+        anim = GetComponent<Animator>();
+        rigi = GetComponent<Rigidbody2D>();
+
+        level_logic = FindObjectOfType<LevelLogic>();
+        platform_controller = GetComponent<PlatformController>();
+    }
+
+    void Start () {
+        remaining_jumps = air_jumps;
+	}
+
+    void FixedUpdate() {
+
+        UpdatePlatformController();
+    }
+
+    private void UpdatePlatformController() {
+        platform_controller.EdgeCheck();
+
+        if (Input.GetButtonDown("Jump")) {
+            platform_controller.UpdateJump();
+        }
+
+        platform_controller.UpdateHorizontalMovement();
+        is_grounded = platform_controller.CheckGrounded();
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+
+        if (coll.gameObject.GetComponent<WinArea>() != null) {
+            print("Win condition!");
+            level_logic.WinCondition();
+        }
+
+        if (is_grounded) {
+            remaining_jumps = air_jumps;
+        }
+    }
+}
