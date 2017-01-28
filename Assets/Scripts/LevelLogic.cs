8d4a723e44ae21e7237265053f6d3b5983a9af09
jakob <jakob.willforss@hotmail.com>
2017-01-28 14:17:08 +0100

    First relatively functional three-level sequence set up

--- Assets/Scripts/LevelLogic.cs
+++ Assets/Scripts/LevelLogic.cs
@@ -5,11 +5,14 @@ using UnityEngine.SceneManagement;
 public class LevelLogic : MonoBehaviour {
 
 
-    public int current_level;
+    // public int current_level;
 
     public void WinCondition() {
 
-        print("Loading scene");
-        SceneManager.LoadScene(current_level + 1);
+
+        var current_level = SceneManager.GetActiveScene().buildIndex;
+        var new_level = current_level + 1;
+        print("Loading scene: " + new_level);
+        SceneManager.LoadScene(new_level);
     }
 }
