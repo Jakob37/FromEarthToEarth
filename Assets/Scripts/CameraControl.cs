ae67bf6f48ce392128b61f311b2f26a62b9838a3
jakob <jakob.willforss@hotmail.com>
2017-02-04 08:29:44 +0100

    Camera successfully limited to level

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -8,6 +8,11 @@ public class CameraControl : MonoBehaviour {
     private float LEFT_EDGE = -1.644665f;
     private float RIGHT_EDGE = 3f;
 
+    private float screen_width = 8f;
+
+    public Transform left_edge;
+    public Transform right_edge;
+
     void Start () {
         player = FindObjectOfType<Player>();
 	}
@@ -28,7 +33,11 @@ public class CameraControl : MonoBehaviour {
 
         transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
 
-        var newX = Mathf.Clamp(transform.position.x, LEFT_EDGE, RIGHT_EDGE);
+        var left_pos = left_edge.position.x;
+        var right_pos = right_edge.position.x;
+
+        var newX = Mathf.Clamp(transform.position.x, left_pos + screen_width / 2, right_pos - screen_width / 2);
+        // var newX = Mathf.Clamp(transform.position.x, LEFT_EDGE, RIGHT_EDGE);
         transform.position = new Vector3(newX, transform.position.y, transform.position.z);
     }
 }
