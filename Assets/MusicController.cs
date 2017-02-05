0313a31c85a90086454445343bff6621fa3307ae
jakob <jakob.willforss@hotmail.com>
2017-02-05 10:20:16 +0100

    First guitar music implemented

---
+++ Assets/MusicController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour {
+
+//    public AudioClip[] audio_clips;
+    public AudioSource first_rain_song;
+
+    private static MusicController instance = null;
+    public static MusicController Instance {
+        get { return instance; }
+    }
+
+    private float transition_in_time;
+    private float transition_out_time;
+
+    void Awake() {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        }
+        else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+	void Start () {
+
+        if (first_rain_song != null) {
+            first_rain_song.Play();
+        }
+        else {
+            Debug.Log("Warning: No song assigned to first_rain_song");
+        }
+    }
+	
+}
