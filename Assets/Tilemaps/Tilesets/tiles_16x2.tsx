cf8188da2a244e0a17de29fd2a1ad1ec9c0a2083
jakob <jakob.willforss@hotmail.com>
2017-02-18 15:18:24 +0100

    Background tiles color edited

---
+++ Assets/Tilemaps/Tilesets/tiles_16x2.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_16x2" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../../../../../../Dropbox/FromEarthToEarth/Doodles/ground/tiles_16x2.png" width="96" height="96"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.0909091" width="32.0909" height="32.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.181818" y="-0.272727" width="32.4545" height="32.5455"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="31.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="1" x="2.09091" y="-0.272727" width="30" height="32.4545"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.454545" width="32.2727" height="32.4545"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.181818" y="0.181818" width="32.1818" height="31.6364"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="1" x="0.272727" y="-0.0909091" width="31.5455" height="31.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="0" width="31.6364" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.272727" width="32.0909" height="32.4545"/>
+  </objectgroup>
+ </tile>
+</tileset>
