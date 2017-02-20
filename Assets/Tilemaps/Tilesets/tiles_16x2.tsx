af7f73fe8260208a8056d46f1efa1b5dbd268535
jakob <jakob.willforss@hotmail.com>
2017-02-20 19:49:13 +0100

    Reorganizing repository. Levels updated to new tilesets. Sprites for small blue flower and player sketch imported.

--- Assets/Tilemaps/Tilesets/tiles_16x2.tsx
+++ Assets/Tilemaps/Tilesets/tiles_16x2.tsx
@@ -1,49 +1,64 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles_16x2" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="../../../../../../Dropbox/FromEarthToEarth/Doodles/ground/tiles_16x2.png" width="96" height="96"/>
+<tileset name="tiles_16x2" tilewidth="32" tileheight="32" tilecount="18" columns="6">
+ <image source="../../../../../../Dropbox/FromEarthToEarth/Doodles/ground/tiles_16x2.png" width="192" height="96"/>
  <tile id="0">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="-0.0909091" width="32.0909" height="32.0909"/>
+   <object id="1" x="-0.181818" y="-0.181818" width="32.2727" height="32.2727"/>
   </objectgroup>
  </tile>
  <tile id="1">
   <objectgroup draworder="index">
-   <object id="1" x="-0.181818" y="-0.272727" width="32.4545" height="32.5455"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="2">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="32" height="31.8182"/>
+   <object id="1" x="0" y="0" width="31.8182" height="32"/>
   </objectgroup>
  </tile>
  <tile id="3">
   <objectgroup draworder="index">
-   <object id="1" x="2.09091" y="-0.272727" width="30" height="32.4545"/>
+   <object id="1" x="-0.181818" y="0.181818" width="32" height="31.6364"/>
   </objectgroup>
  </tile>
  <tile id="4">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="-0.454545" width="32.2727" height="32.4545"/>
+   <object id="7" x="-0.0909091" y="0" width="32.0909" height="32"/>
   </objectgroup>
  </tile>
  <tile id="5">
   <objectgroup draworder="index">
-   <object id="1" x="-0.181818" y="0.181818" width="32.1818" height="31.6364"/>
+   <object id="1" x="0" y="-0.181818" width="32.0909" height="32"/>
   </objectgroup>
  </tile>
  <tile id="6">
   <objectgroup draworder="index">
-   <object id="1" x="0.272727" y="-0.0909091" width="31.5455" height="31.8182"/>
+   <object id="1" x="1.81818" y="0" width="30.1818" height="32.2727"/>
   </objectgroup>
  </tile>
  <tile id="7">
   <objectgroup draworder="index">
-   <object id="1" x="0.181818" y="0" width="31.6364" height="32"/>
+   <object id="1" x="-0.181818" y="-0.272727" width="32" height="32.2727"/>
   </objectgroup>
  </tile>
  <tile id="8">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="-0.272727" width="32.0909" height="32.4545"/>
+   <object id="1" x="0" y="0" width="31.8182" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.181818" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="0" width="31.8182" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.181818" width="32" height="32.1818"/>
   </objectgroup>
  </tile>
 </tileset>
