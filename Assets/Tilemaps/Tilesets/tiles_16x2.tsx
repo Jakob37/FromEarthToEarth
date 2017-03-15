5ef8519b4f934b590b11bf850474e2071eb43b74
jakob <jakob.willforss@hotmail.com>
2017-03-15 18:20:15 +0100

    Update tilesets for all levels. Even out collision boxes. Add vines.

--- Assets/Tilemaps/Tilesets/tiles_16x2.tsx
+++ Assets/Tilemaps/Tilesets/tiles_16x2.tsx
@@ -1,64 +1,65 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles_16x2" tilewidth="32" tileheight="32" tilecount="18" columns="6">
- <image source="../../../../../../Dropbox/FromEarthToEarth/Doodles/ground/tiles_16x2.png" width="192" height="96"/>
+<tileset name="tiles_16_with_vines" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <image source="tiles_16_with_vines.png" width="192" height="192"/>
  <tile id="0">
-  <objectgroup draworder="index">
-   <object id="1" x="-0.181818" y="-0.181818" width="32.2727" height="32.2727"/>
-  </objectgroup>
- </tile>
- <tile id="1">
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="0" width="32" height="32"/>
+   <object id="5" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="2">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="31.8182" height="32"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="3">
   <objectgroup draworder="index">
-   <object id="1" x="-0.181818" y="0.181818" width="32" height="31.6364"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="4">
   <objectgroup draworder="index">
-   <object id="7" x="-0.0909091" y="0" width="32.0909" height="32"/>
+   <object id="1" x="0.25" y="0.25" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="5">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="-0.181818" width="32.0909" height="32"/>
+   <object id="1" x="0.25" y="0.25" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="6">
   <objectgroup draworder="index">
-   <object id="1" x="1.81818" y="0" width="30.1818" height="32.2727"/>
+   <object id="2" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="7">
   <objectgroup draworder="index">
-   <object id="1" x="-0.181818" y="-0.272727" width="32" height="32.2727"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="8">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="31.8182" height="32"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="12">
   <objectgroup draworder="index">
-   <object id="1" x="-0.181818" y="0" width="32" height="32"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="13">
   <objectgroup draworder="index">
-   <object id="1" x="0.181818" y="0" width="31.8182" height="32"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="14">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="-0.181818" width="32" height="32.1818"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
 </tileset>
