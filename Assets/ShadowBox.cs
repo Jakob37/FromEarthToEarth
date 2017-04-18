22e98f5018d98cead16b9a94dd8fe29d4fc4f4d4
jakob <jakob.willforss@hotmail.com>
2017-04-18 20:54:13 +0200

    Slow progress on positioning shadow overlay

--- Assets/ShadowBox.cs
+++ Assets/ShadowBox.cs
@@ -1,10 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Tiled2Unity;
 
 public class ShadowBox : MonoBehaviour {
 
+    private SpriteRenderer renderer;
+
+
+    private float tile_size;
+    private float scale_factor;
+    public float Size {
+        get {
+            return tile_size * scale_factor;
+        }
+    }
+
+    void Awake() {
+        renderer = GetComponent<SpriteRenderer>();
+        TiledMap tiled_map = GameObject.FindObjectOfType<TiledMap>();
+        tile_size = tiled_map.TileWidth;
+        //scale_factor = tile_size * tiled_map.ExportScale * 1.6f;
+        scale_factor = renderer.sprite.pixelsPerUnit;
+
+        print("shadow box");
+
+        renderer.transform.localScale = new Vector2(32/scale_factor, 32/scale_factor);
+    }
+
 	void Start () {
-	
+
 	}
 	
 	void Update () {
--- Assets/ShadowOverlay.cs
+++ Assets/ShadowOverlay.cs
@@ -6,16 +6,31 @@ public class ShadowOverlay : MonoBehaviour {
     private GameObject[] shadow_boxes;
 
     public GameObject shadow_box_prefab;
+    public GameObject level_tiles_object;
 
     private LeftEdge left_edge;
     private RightEdge right_edge;
 
     private float box_size;
 
+    private Vector2 Origin {
+        get {
+            return new Vector2(level_tiles_object.transform.position.x, 0);
+            // return level_tiles_object.transform.position;
+        }
+    }
 
 	void Start () {
 
-        box_size = shadow_box_prefab.GetComponent<SpriteRenderer>().sprite.texture.width / shadow_box_prefab.GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
+        SpriteRenderer box_renderer = shadow_box_prefab.GetComponent<SpriteRenderer>();
+
+        // box_size = box_renderer.sprite.texture.width * shadow_box_prefab.transform.localScale.x / box_renderer.sprite.pixelsPerUnit;
+        //box_size = shadow_box_prefab.GetComponent<SpriteRenderer>().sprite.texture.width / shadow_box_prefab.GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
+
+        ShadowBox shadow_box_script = box_renderer.gameObject.GetComponent<ShadowBox>();
+
+        box_size = box_renderer.gameObject.GetComponent<ShadowBox>().Size;
+        box_size = 32f / 100f;
 
         left_edge = GameObject.FindObjectOfType<LeftEdge>();
         right_edge = GameObject.FindObjectOfType<RightEdge>();
@@ -24,7 +39,12 @@ public class ShadowOverlay : MonoBehaviour {
 
         for (int i = 0; i < shadow_boxes.Length; i++) {
             shadow_boxes[i] = (GameObject)Instantiate(shadow_box_prefab);
-            shadow_boxes[i].transform.position = new Vector2(left_edge.transform.position.x + i * box_size, 0);
+
+            // float box_size = 10;
+
+            // shadow_boxes[i].transform.position = new Vector2(left_edge.transform.position.x + i * box_size, 0);
+            shadow_boxes[i].transform.position = new Vector2(Origin.x + i * box_size, 0);
+            print(shadow_boxes[i].transform.position);
             print("Created at: " + shadow_boxes[i].transform.position);
         }
 
