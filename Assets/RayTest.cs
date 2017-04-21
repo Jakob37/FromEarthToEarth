6c3c596edac83c7d4637e2a99251071434b8b601
jakob <jakob.willforss@hotmail.com>
2017-04-21 18:30:04 +0200

    Basic drawn line debug system for linecast

--- Assets/RayTest.cs
+++ Assets/RayTest.cs
@@ -8,23 +8,28 @@ public class RayTest : MonoBehaviour {
     private Transform target;
 
 	void Start () {
-        target = GameObject.FindObjectOfType<RayTarget>().gameObject.transform;
-
+        target = GameObject.FindObjectOfType<Player>().gameObject.transform;
         print("Firing ray!");
-
         // Partly based on: http://answers.unity3d.com/questions/840614/linecast-detecting-collision-from-target.html
+    }
+	
+	void Update () {
 
+        Debug.DrawLine(new Vector3(-5, 0, 0), new Vector3(1, 0, 0), Color.red, 2, false);
+
+        if (Input.GetKeyDown(KeyCode.L)) {
+            DoLinecast();
+        }
+	}
+
+    private void DoLinecast() {
         RaycastHit hit;
         //var hit : Raycasthit;
 
+        Debug.DrawLine(target.position, transform.position, Color.yellow, 2, false);
         var output = Physics.Linecast(transform.position, target.position, out hit, hit_mask);
 
         print(output);
         print(hit.point);
-
     }
-	
-	void Update () {
-	
-	}
 }
