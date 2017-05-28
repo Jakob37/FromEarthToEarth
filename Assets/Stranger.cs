4c8501b2bb731fab6280a25aa102799c089096ce
jakob <jakob.willforss@hotmail.com>
2017-05-28 19:33:36 +0200

    First stranger draft in place

---
+++ Assets/Stranger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stranger : MonoBehaviour {
+
+    private Collider2D player_coll;
+    private Collider2D coll;
+
+    void Start() {
+        player_coll = FindObjectOfType<Player>().gameObject.GetComponent<Collider2D>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+
+        if (collision.gameObject.GetComponent<Player>() != null) {
+            Physics2D.IgnoreCollision(collision.collider, coll);
+        }
+    }
+}
