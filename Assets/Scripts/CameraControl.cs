8ac129e1da921a06160c8ce3888b299b9eb6ca8d
jakob <jakob.willforss@hotmail.com>
2017-05-31 19:32:40 +0200

    Option to navigate height-wise with following camera

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -6,9 +6,12 @@ public class CameraControl : MonoBehaviour {
     private Player player;
 
     private float screen_width = 8f;
+    private float screen_height = 6f;
 
     public Transform left_edge;
     public Transform right_edge;
+    public Transform top_edge;
+    public Transform bottom_edge;
 
     void Start () {
         player = FindObjectOfType<Player>();
@@ -28,13 +31,19 @@ public class CameraControl : MonoBehaviour {
 
     private void TrackPlayer() {
 
-        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
 
         var left_pos = left_edge.position.x;
         var right_pos = right_edge.position.x;
 
+        var top_pos = top_edge.position.y;
+        var bottom_pos = bottom_edge.position.y;
+
+        print(top_pos);
+        print(bottom_pos);
+
         var newX = Mathf.Clamp(transform.position.x, left_pos + screen_width / 2, right_pos - screen_width / 2);
-        // var newX = Mathf.Clamp(transform.position.x, LEFT_EDGE, RIGHT_EDGE);
-        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        var newY = Mathf.Clamp(transform.position.y, bottom_pos + screen_height / 2, top_pos - screen_height / 2);
+        transform.position = new Vector3(newX, newY, transform.position.z);
     }
 }
