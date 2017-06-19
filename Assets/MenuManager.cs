ee5ee06ee571137dee31f83349a727d802ff06d7
jakob <jakob.willforss@hotmail.com>
2017-06-19 10:29:27 +0200

    Menu states and navigation between

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject main_canvas;
+    public GameObject level_pick_canvas;
+
+    void Start() {
+
+        main_canvas.SetActive(true);
+        level_pick_canvas.SetActive(false);
+    }
+
+    public void SwitchMenuState(bool level_pick_active) {
+        if (level_pick_active) {
+            level_pick_canvas.SetActive(true);
+            main_canvas.SetActive(false);
+        }
+        else {
+            level_pick_canvas.SetActive(false);
+            main_canvas.SetActive(true);
+        }
+    }
+
+    public void SwitchScene(int level_index) {
+        SceneManager.LoadScene(level_index);
+    }
+
+}
