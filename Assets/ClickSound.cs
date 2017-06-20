270f9e0dd0cbac23b1eb9030de4b66c733e4f039
jakob <jakob.willforss@hotmail.com>
2017-06-20 11:16:38 +0200

    Add menu sound effect and basic sound effect system

---
+++ Assets/ClickSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickSound : MonoBehaviour {
+
+    private Button button { get { return GetComponent<Button>(); } }
+
+    void Start() {
+
+        button.onClick.AddListener(() => PlaySound());
+    }
+
+    void PlaySound() {
+
+        var sound_manager = FindObjectOfType<SoundEffectManager>();
+        sound_manager.PlaySound(SoundEffect.basic_click);
+    }
+}
---
+++ Assets/SoundEffectManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SoundEffect {
+    basic_click
+}
+
+public class SoundEffectManager : MonoBehaviour {
+
+    public AudioSource basic_click;
+
+    void Start() {
+
+    }
+
+    public void PlaySound(SoundEffect sound_effect) {
+        switch (sound_effect) {
+            case SoundEffect.basic_click:
+                basic_click.Play();
+                break;
+            default:
+                break;
+        }
+    }
+
+}
