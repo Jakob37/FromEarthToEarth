05956f34cb4c489aeac371f6a694299c0ed20751
jakob <jakob.willforss@hotmail.com>
2017-12-31 12:45:05 +0100

    Fading indicator text

--- Assets/Scripts/ControlText.cs
+++ Assets/Scripts/ControlText.cs
@@ -1,31 +1,100 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlText : MonoBehaviour {
 
-    public GameObject text_object;
+    public enum ControlTextState {
+        inactive,
+        active,
+        indicator
+    }
+
+    public GameObject text_object_full;
+    public GameObject text_object_short;
     public bool start_active;
 
+    private Text full_text;
+    private Text short_text;
+
+    public float total_fade_s;
+    private float current_fade_s;
+
+    public float start_fade_delay_s;
+
+    private ControlTextState active_state;
+
     void Start() {
+
+        full_text = text_object_full.GetComponent<Text>();
+        short_text = text_object_short.GetComponent<Text>();
+
         if (start_active) {
-            SetActive(true);
+            SetActiveState(ControlTextState.indicator);
+            current_fade_s = total_fade_s;
         }
         else {
-            SetActive(false);
+            SetActiveState(ControlTextState.inactive);
+            current_fade_s = 0;
         }
+    }
+
+    void Update() {
+
+        if (start_fade_delay_s > 0) {
+            start_fade_delay_s -= Time.deltaTime;
+        }
+
+        if (active_state == ControlTextState.indicator && start_fade_delay_s <= 0) {
+            if (current_fade_s > 0) {
+                FadeOut();
+            }
+            else {
+                print("Inactivated");
+                SetActiveState(ControlTextState.inactive);
+            }
+        }
+        else {
+            SetTextAlpha(1);
+        }
+    }
+
+    private void FadeOut() {
+        current_fade_s -= Time.deltaTime;
+        float current_alpha = current_fade_s / total_fade_s;
+        SetTextAlpha(current_alpha);
+    }
+
+    private void SetTextAlpha(float alpha) {
+        full_text.color = new Color(1, 1, 1, alpha);
+        short_text.color = new Color(1, 1, 1, alpha);
     }
 
     public void ToggleActive() {
-        if (text_object.activeSelf) {
-            SetActive(false);
+        if (active_state == ControlTextState.active) {
+            SetActiveState(ControlTextState.inactive);
         }
         else {
-            SetActive(true);
+            SetActiveState(ControlTextState.active);
         }
     }
 
-    public void SetActive(bool is_active) {
-        text_object.SetActive(is_active);
+    public void SetActiveState(ControlTextState state) {
+
+        active_state = state;
+
+        if (state == ControlTextState.active) {
+            text_object_full.SetActive(true);
+            text_object_short.SetActive(false);
+        }
+        else if (state == ControlTextState.indicator) {
+            text_object_full.SetActive(false);
+            text_object_short.SetActive(true);
+        }
+        else {
+            text_object_full.SetActive(false);
+            text_object_short.SetActive(false);
+        }
     }
 }
