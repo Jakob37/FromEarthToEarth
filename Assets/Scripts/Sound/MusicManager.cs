cca845efeecede5560cf472c5d0d757091d1bc2d
jakob <jakob.willforss@hotmail.com>
2018-02-22 19:18:33 +0100

    Looping sound with pause in place

--- Assets/Scripts/Sound/MusicManager.cs
+++ Assets/Scripts/Sound/MusicManager.cs
@@ -16,6 +16,7 @@ public class MusicManager : MonoBehaviour {
 
     public AudioSource vivid_skies;
     public AudioSource steady_rain;
+    public int time_between_sounds;
 
     private List<Song> played_songs;
 
@@ -34,15 +35,42 @@ public class MusicManager : MonoBehaviour {
     }
 
     void Start() {
+
+        if (time_between_sounds == 0) {
+            time_between_sounds = 10;
+            print("No time found, time between sounds assigned 10 to avoid infinite looop");
+        }
+
         current_song = null;
         played_songs = new List<Song>();
+
+        StartCoroutine("SoundLoop");
     }
+
+    private IEnumerator SoundLoop() {
+
+        while (true) { //or for(;;){
+
+            if (!instance.vivid_skies.isPlaying) {
+                yield return new WaitForSeconds(time_between_sounds);
+                instance.vivid_skies.Play();
+            }
+            else {
+                yield return new WaitForSeconds(1);
+            }
+        }
+    }
+
+
+    // private IEnumerator Pause() {
+    //     yield return new WaitForSeconds(5);
+    // }
 
     public void TrigNewLevel() {
 
-        if (instance.current_song == null) {
-            instance.current_song = instance.vivid_skies;
-            instance.current_song.Play();
+        if (!instance.vivid_skies.isPlaying) {
+            // instance.current_song = instance.vivid_skies;
+            instance.vivid_skies.Play();
             instance.played_songs.Add(Song.vivid_skies);
         }
     }
